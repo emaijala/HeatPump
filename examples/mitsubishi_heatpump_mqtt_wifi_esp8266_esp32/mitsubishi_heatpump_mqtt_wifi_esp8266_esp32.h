a4ee0ede57e365769c6d58a0266ad2269dcb9531
Ere Maijala <ere@labs.fi>
2019-10-28 23:29:07 +0200

    Add an extended mqtt version with WifiManager etc.

---
+++ examples/mitsubishi_heatpump_mqtt_wifi_esp8266_esp32/mitsubishi_heatpump_mqtt_wifi_esp8266_esp32.h
@@ -0,0 +1,29 @@
+
+//#define ESP32
+#define OTA
+const char* ota_password = "otaxotax";
+
+// mqtt server settings
+char* mqtt_server   = "192.168.8.201";
+char* mqtt_port     = "1883";
+char* mqtt_username = "mqtt";
+char* mqtt_password = "mqttpass";
+
+// mqtt client settings
+// Note PubSubClient.h has a MQTT_MAX_PACKET_SIZE of 128 defined, so either raise it to 256 or use short topics
+const char* client_id                   = "heatpump"; // Must be unique on the MQTT network
+const char* heatpump_topic              = "heatpump";
+const char* heatpump_set_topic          = "heatpump/set";
+const char* heatpump_rtemp_set_topic    = "heatpump/set/rtemp";
+const char* heatpump_status_topic       = "heatpump/status";
+const char* heatpump_timers_topic       = "heatpump/timers";
+
+const char* heatpump_debug_topic        = "heatpump/debug";
+const char* heatpump_debug_set_topic    = "heatpump/debug/set";
+
+// pinouts
+const int redLedPin  = 0; // Onboard LED = digital pin 0 (red LED on adafruit ESP8266 huzzah)
+const int blueLedPin = 2; // Onboard LED = digital pin 0 (blue LED on adafruit ESP8266 huzzah)
+
+// sketch settings
+const unsigned int SEND_ROOM_TEMP_INTERVAL_MS = 60000;
